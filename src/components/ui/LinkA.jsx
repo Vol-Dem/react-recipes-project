629348bc4374254576c514a7e2b95829b7179f97
Vol-Dem <saten898@gmail.com>
2026-08-25 03:23:35 +0300

    refactor: extract inline icons into assets

--- src/components/ui/LinkA.jsx
+++ src/components/ui/LinkA.jsx
@@ -1,4 +1,5 @@
 import classes from "./LinkA.module.scss";
+import ExternalLinkIcon from "../../assets/ExternalLinkIcon";
 
 const LinkA = ({
   children,
@@ -33,22 +34,7 @@ const LinkA = ({
       }}
     >
       {children}
-      {external && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
-          />
-        </svg>
-      )}
+      {external && <ExternalLinkIcon className="w-6 h-6" />}
     </a>
   );
 };
--- src/components/ui/SuccessMessage.jsx
+++ src/components/ui/SuccessMessage.jsx
@@ -1,22 +1,10 @@
 import classes from "./SuccessMessage.module.scss";
+import SuccessIcon from "../../assets/SuccessIcon";
 
 const SuccessMessage = ({ children, className }) => {
   return (
     <div className={`${classes["success"]} ${className || ""}`}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-6 h-6"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-        />
-      </svg>
+      <SuccessIcon className="w-6 h-6" />
       <span>{children}</span>
     </div>
   );
