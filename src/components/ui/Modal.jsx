14400c45afd6a5728afb149c3869f81da22b9beb
Vol-Dem <saten898@gmail.com>
2026-08-26 04:14:49 +0300

    fix: correct portal return syntax

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -18,51 +18,50 @@ const Modal = ({ children, className, disableClass, onClose, title }) => {
   }, []);
 
   return createPortal(
-        <div className={`${disableClass || ""}`}>
-          <motion.div
-            variants={{
-              hidden: { opacity: 0 },
-              visible: { opacity: 1 },
-              exit: { opacity: 0 },
-            }}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            className={`${classes.modal} ${classes["modal--backdrop"]}`}
+    <div className={`${disableClass || ""}`}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0 },
+          visible: { opacity: 1 },
+          exit: { opacity: 0 },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className={`${classes.modal} ${classes["modal--backdrop"]}`}
+        onClick={onClose}
+      ></motion.div>
+      <motion.div
+        role="dialog"
+        aria-modal="true"
+        aria-label={title || "Dialog"}
+        layout
+        variants={{
+          hidden: { opacity: 0, y: "-30%", x: "-50%" },
+          visible: { opacity: 1, y: "-50%", x: "-50%" },
+          exit: { opacity: 0, y: "-30%", x: "-50%" },
+        }}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className={`${classes.modal} ${classes["modal--content"]} ${
+          className ? className : ""
+        }`}
+      >
+        <Card>
+          {title && <h2 className={classes.title}>{title}</h2>}
+          {children}
+          <button
+            className={classes["modal__close"]}
             onClick={onClose}
-          ></motion.div>
-          <motion.div
-            role="dialog"
-            aria-modal="true"
-            aria-label={title || "Dialog"}
-            layout
-            variants={{
-              hidden: { opacity: 0, y: "-30%", x: "-50%" },
-              visible: { opacity: 1, y: "-50%", x: "-50%" },
-              exit: { opacity: 0, y: "-30%", x: "-50%" },
-            }}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            className={`${classes.modal} ${classes["modal--content"]} ${
-              className ? className : ""
-            }`}
+            aria-label="Close dialog"
           >
-            <Card>
-              {title && <h2 className={classes.title}>{title}</h2>}
-              {children}
-              <button
-                className={classes["modal__close"]}
-                onClick={onClose}
-                aria-label="Close dialog"
-              >
-                <CloseIcon />
-              </button>
-            </Card>
-          </motion.div>
-        </div>,
-        document.body
-      )
+            <CloseIcon />
+          </button>
+        </Card>
+      </motion.div>
+    </div>,
+    document.body,
   );
 };
 
--- src/components/ui/Notification.jsx
+++ src/components/ui/Notification.jsx
@@ -12,31 +12,31 @@ const Notification = ({ title, message }) => {
   const closeNotificationHandler = () => {
     dispatch(notificationActions.closeNotification());
   };
+
   return createPortal(
-        <motion.div
-          role="status"
-          aria-live="polite"
-          initial={{ opacity: 0, y: 30, x: "-50%" }}
-          animate={{ opacity: 1, y: 0, x: "-50%" }}
-          exit={{ opacity: 0, y: 30, x: "-50%" }}
-          className={classes["notification-container"]}
+    <motion.div
+      role="status"
+      aria-live="polite"
+      initial={{ opacity: 0, y: 30, x: "-50%" }}
+      animate={{ opacity: 1, y: 0, x: "-50%" }}
+      exit={{ opacity: 0, y: 30, x: "-50%" }}
+      className={classes["notification-container"]}
+    >
+      <Card className={classes.notification}>
+        <TriangleIcon />
+        <div>
+          <h4 className={classes["notification__title"]}>{title}</h4>
+          <p className={classes["notification__text"]}>{message}</p>
+        </div>
+        <Button
+          className={classes["notification__btn"]}
+          onClick={closeNotificationHandler}
         >
-          <Card className={classes.notification}>
-            <TriangleIcon />
-            <div>
-              <h4 className={classes["notification__title"]}>{title}</h4>
-              <p className={classes["notification__text"]}>{message}</p>
-            </div>
-            <Button
-              className={classes["notification__btn"]}
-              onClick={closeNotificationHandler}
-            >
-              Got it!
-            </Button>
-          </Card>
-        </motion.div>,
-        document.body,
-      )
+          Got it!
+        </Button>
+      </Card>
+    </motion.div>,
+    document.body,
   );
 };
 
