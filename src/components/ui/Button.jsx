48815d0ac91508d20fce43a1ccfb560d688838c7
Vol-Dem <saten898@gmail.com>
2026-08-26 04:06:39 +0300

    refactor: simplify button prop forwarding

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,15 +1,13 @@
 import classes from "./Button.module.scss";
 import { motion } from "framer-motion";
 
-const Button = ({ button, children, className, disabled, onClick }) => {
+const Button = ({ children, className, ...buttonProps }) => {
   return (
     <motion.button
+      {...buttonProps}
       whileHover={{ scale: 1.1, transition: { type: "spring" } }}
       whileTap={{ scale: 0.95 }}
       className={`${classes.btn} ${className || ""}`}
-      onClick={onClick}
-      disabled={disabled}
-      {...button}
     >
       {children}
     </motion.button>
